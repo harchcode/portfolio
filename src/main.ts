6e0c1b7d559b8f2dd4d491cb410a1e282d01f1cf
Hartono Chandra <harchcode@gmail.com>
2021-12-30 22:53:04 +0700

    menu links and scrolling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,85 @@
+import { tween } from "./utils";
+
+const menuAnchors: HTMLElement[] = [];
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  menuAnchors.push(anchor as HTMLElement);
+});
+
+const menuAnchorMap: Record<string, HTMLElement> = {};
+const menuDivs: HTMLElement[] = [];
+const menuDivMap: Record<string, HTMLElement> = {};
+
+const ACTIVE_MENU_CLASS = "active";
+
+function windowScrollY(to: number) {
+  window.scrollTo(window.scrollX, to);
+}
+
+menuAnchors.forEach(anchor => {
+  const href = anchor.getAttribute("href");
+  const id = href.substr(1);
+
+  menuAnchorMap[id] = anchor;
+
+  const div = document.getElementById(id);
+  menuDivs.push(div);
+  menuDivMap[id] = div;
+
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const target = anchor.getAttribute("href");
-    if (!target) return;
-
-    const targetEl = document.querySelector(target);
-    if (!targetEl) return;
-
-    targetEl.scrollIntoView({
-      behavior: "smooth"
-    });
+    tween(window.scrollY, div.offsetTop, 1000, windowScrollY);
   });
 });
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function getActiveMenuId(scrollPos: number): string {
+  for (let i = menuDivs.length - 1; i >= 0; i--) {
+    const menuDiv = menuDivs[i] as HTMLElement;
+
+    if (scrollPos >= menuDiv.offsetTop - 300) {
+      return menuDiv.id;
+    }
+  }
+
+  return "";
+}
+
+function setActiveMenu(id: string) {
+  for (const anchor of menuAnchors) {
+    anchor.classList.remove(ACTIVE_MENU_CLASS);
+  }
+
+  menuAnchorMap[id].classList.add(ACTIVE_MENU_CLASS);
+}
+
+function doSomething(scrollPos: number) {
+  setActiveMenu(getActiveMenuId(scrollPos));
+}
+
+window.addEventListener("scroll", () => {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+function initPage() {
+  const tmp = window.location.hash ? window.location.hash.substr(1) : "about";
+
+  setActiveMenu(tmp);
+
+  setTimeout(() => {
+    menuDivMap[tmp].scrollIntoView();
+  });
+}
+
+initPage();
