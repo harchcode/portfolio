0a4be8c04f8c445fd13d4023b729f00e6011ad99
Hartono Chandra <harchcode@gmail.com>
2022-11-27 20:17:40 +0700

    responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{astro,ts,css}"],
   theme: {
-    extend: {}
+    extend: {
+      width: {
+        128: "32rem",
+        160: "40rem"
+      },
+      height: {
+        128: "32rem",
+        160: "40rem"
+      },
+      screens: {
+        sm: "480px"
+        // => @media (min-width: 992px) { ... }
+      }
+    }
   },
   plugins: []
 };
