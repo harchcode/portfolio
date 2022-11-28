fbc5bac21f240ac1ec417d231fe6534fedf6f915
Hartono Chandra <harchcode@gmail.com>
2022-11-28 19:26:58 +0700

    adjust colors

---
+++ README.md
@@ -0,0 +1,1 @@
+Color Pallete: https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,12 @@ module.exports = {
       },
       screens: {
         sm: "480px"
-        // => @media (min-width: 992px) { ... }
+      },
+      colors: {
+        color1: "#F8EDE3",
+        color2: "#DFD3C3",
+        color3: "#D0B8A8",
+        color4: "#7D6E83"
       }
     }
   },
