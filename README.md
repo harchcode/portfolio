822d6169e6d7917ea552c543b02c3d83b8a8131d
Hartono Chandra <harchcode@gmail.com>
2022-11-28 20:05:25 +0700

    init dark mode

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Color Pallete: https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
+Color Pallete:
+
+- https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
+- https://colorhunt.co/palette/2c36393f4e4fa27b5cdcd7c9
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,7 +18,11 @@ module.exports = {
         color1: "#F8EDE3",
         color2: "#DFD3C3",
         color3: "#D0B8A8",
-        color4: "#7D6E83"
+        color4: "#7D6E83",
+        dcolor1: "#2C3639",
+        dcolor2: "#3F4E4F",
+        dcolor3: "#A27B5C",
+        dcolor4: "#DCD7C9"
       }
     }
   },
