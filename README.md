4012eadb7138ec3ed25ebe90314c016f4829931c
Hartono Chandra <harchcode@gmail.com>
2022-11-30 21:26:45 +0700

    handle dark mode

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Color Pallete:
 
 - https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
 - https://colorhunt.co/palette/2c36393f4e4fa27b5cdcd7c9
+
+Icons: https://ionic.io/ionicons
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,67 @@
 import { initBackground } from "./background";
 
+const systemModeButton = document.getElementById(
+  "system-mode-button"
+) as HTMLButtonElement;
+
+const lightModeButton = document.getElementById(
+  "light-mode-button"
+) as HTMLButtonElement;
+
+const darkModeButton = document.getElementById(
+  "dark-mode-button"
+) as HTMLButtonElement;
+
+function clearSelectedTheme() {
+  systemModeButton.classList.remove("active");
+  lightModeButton.classList.remove("active");
+  darkModeButton.classList.remove("active");
+}
+
+function initSelectedTheme() {
+  clearSelectedTheme();
+
+  if (!localStorage.theme) {
+    systemModeButton.classList.add("active");
+  } else if (localStorage.theme === "light") {
+    lightModeButton.classList.add("active");
+  } else if (localStorage.theme === "dark") {
+    darkModeButton.classList.add("active");
+  }
+}
+
 function main() {
+  initSelectedTheme();
   initBackground();
+
+  systemModeButton.addEventListener("click", () => {
+    localStorage.removeItem("theme");
+
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    clearSelectedTheme();
+    systemModeButton.classList.add("active");
+  });
+
+  lightModeButton.addEventListener("click", () => {
+    localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+
+    clearSelectedTheme();
+    lightModeButton.classList.add("active");
+  });
+
+  darkModeButton.addEventListener("click", () => {
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+
+    clearSelectedTheme();
+    darkModeButton.classList.add("active");
+  });
 }
 
 main();
