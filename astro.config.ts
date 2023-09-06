9a53ba2da663d577ac24443737155ffb5ace2960
Hartono Chandra <harchcode@gmail.com>
2023-09-06 19:53:38 +0700

    initial commit

--- astro.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-  server: option => ({
-    port: option.command === "dev" ? 3720 : 5720
-  })
-});
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,ts}"], // intentionally excluding index.html
+  plugins: []
+};
