32034265077ef54ee5429af3db029d54737b175e
Hartono Chandra <harchcode@gmail.com>
2023-09-12 08:53:04 +0700

    update (temp)

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
 import { wait } from "./gaguna/misc";
 import profileSrc from "./assets/profile.svg";
 import { loadImage } from "./gaguna/loader";
+import siteInfo from "./site-info";
 
 async function main() {
   if (!("timeStart" in window) || typeof window.timeStart !== "number") return;
@@ -23,6 +24,39 @@ async function main() {
   const imgEl = await loadImage(profileSrc);
   console.log(imgEl);
 
+  const svgDefEls = siteInfo.svgDefs.map(svgDef => {
+    const el = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    el.style.display = "none";
+    el.setAttribute("version", "2.0");
+
+    el.innerHTML = `<defs>
+        <symbol id="${svgDef.id}" viewbox="${svgDef.viewBox}">
+          <path fill="#000" d="${svgDef.path}"></path>
+        </symbol>
+      </defs>
+      <use href="#${svgDef.id}"></use>`;
+
+    return el;
+  });
+
+  const socialButton =
+    contentElement.getElementsByClassName("social-button")[0];
+
+  siteInfo.socialLinks.forEach((socialLink, i) => {
+    const el =
+      i === 0 ? socialButton : (socialButton.cloneNode(true) as HTMLElement);
+
+    el.setAttribute("href", socialLink.url);
+    el.setAttribute("aria-label", socialLink.label);
+
+    const useEl = el.getElementsByTagName("use")[0];
+    useEl.setAttribute("href", `#${socialLink.svgSymbolId}`);
+
+    if (i > 0) {
+      socialButton.parentNode?.appendChild(el);
+    }
+  });
+
   const endTime = Date.now();
   const elapsed = endTime - startTime;
 
@@ -36,6 +70,10 @@ async function main() {
 
   removeElement(loadingDiv);
   removeElementById("loading-style");
+
+  for (const svgDefEl of svgDefEls) {
+    document.body.appendChild(svgDefEl);
+  }
 
   document.body.appendChild(contentElement);
 }
