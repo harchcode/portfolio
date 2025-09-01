c6270c2d599d40617c2deadf59e46079491c0473
Hartono Chandra <harchcode@gmail.com>
2025-09-01 22:54:09 +0700

    update

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
